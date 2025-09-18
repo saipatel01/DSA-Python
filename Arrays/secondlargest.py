135d450575deb60ce651755f2408c47b13f150c1
saipatel01 <saipatel01.ai@gmail.com>
2025-09-19 00:13:15 +0530

    added second largest element in an array

---
+++ Arrays/secondlargest.py
@@ -0,0 +1,17 @@
+def print2largest( arr):
+    n=len(arr)
+    if n<2:
+        return -1
+    first=second=float('-inf')
+    for num in arr:
+        if num>first:
+            second=first
+            first=num
+        elif num>second and num!=first:
+            second=num
+
+    if second ==float('-inf'):
+       return -1
+    else:
+        return second
+print(print2largest([12, 35, 1, 10, 34, 1]))

60985980920c675912aa8fa79438fb2beab387e5
saipatel01 <saipatel01.ai@gmail.com>
2026-07-02 21:33:17 +0530

    Added Sort colors problems

---
+++ Arrays/SortColors.py
@@ -0,0 +1,23 @@
+def sortColors(nums):
+    low = 0
+    mid = 0
+    high = len(nums) - 1
+
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+
+        elif nums[mid] == 1:
+            mid += 1
+
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+
+nums = [2, 0, 2, 1, 1, 0]
+
+print("Before:", nums)
+sortColors(nums)
+print("After :", nums)
